f5717f8b1a8aaae71e64f32ea511bb91669c9804
Xavier Bucchiotty <xbucchiotty@xebia.fr>
2012-02-01 16:25:18 +0100

    Correction installation Addon Insito

--- git/insito/install-addon.sh
+++ git/insito/install-addon.sh
@@ -9,7 +9,7 @@ git config --local alias.safe-push '!sh '${LINK_DIR}'/git/safe-push/safe-push.sh
 git config --local alias.remote-safe-push '!sh '${LINK_DIR}'/git/insito/remote-safe-push.sh'
 
 if [ 1 -eq $# ]; then
-    $LINK_DIR/git/safe-push/install-remote-run-poste.sh $1
+    sh $LINK_DIR/git/safe-push/install-remote-run-poste.sh $1
 else
     echo "Il faut donner en paramètre du script le nom de votre VM perso pour configurer le remote-run"
     exit 1
