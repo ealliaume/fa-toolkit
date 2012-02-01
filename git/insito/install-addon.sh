446b5e92ada6a3e250f9313ac773a562643f1a34
Xavier Bucchiotty <xbucchiotty@xebia.fr>
2012-02-01 15:47:09 +0100

    ajout install-addon pour insito

---
+++ git/insito/install-addon.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+LINK_DIR="$HOME/.fa-toolkit"
+
+echo "Configuration du pull en rebase"
+git config --local branch.patch.rebase true
+git config --local branch.master.rebase true
+
+git config --local alias.safe-push '!sh '${LINK_DIR}'/git/safe-push/safe-push.sh'
+
+if [ 1 -eq $# ]; then
+    $LINK_DIR/git/safe-push/install-remote-run-poste.sh $1
+else
+    echo "Il faut donner en paramètre du script le nom de votre VM perso pour configurer le remote-run"
+    exit 1
+fi
