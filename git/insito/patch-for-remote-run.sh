4b93cae25a4580d2872053558c3c08ea7643aa6c
Christophe Amory <camory@financeactive.com>
2012-02-02 18:03:30 +0100

    mise à jours des sources avant installation de protobuffer

--- git/insito/patch-for-remote-run.sh
+++ git/insito/patch-for-remote-run.sh
@@ -8,9 +8,9 @@ sudo apt-get update && apt-get install git
 log "Installation de Protobuff"
 
 if [ 0 -eq `grep http://rodolphe.quiedeville.org/debian /etc/apt/sources.list | wc -l `]; then
-	sudo sh -c 'echo "http://rodolphe.quiedeville.org/debian squeeze-backports main" >> /etc/apt/sources.list'
+	sudo sh -c 'echo "deb http://rodolphe.quiedeville.org/debian squeeze-backports main" >> /etc/apt/sources.list'
 fi
-sudo  apt-get install libprotobuf-dev protobuf-compiler
+sudo apt-get update && apt-get install libprotobuf-dev protobuf-compiler
 
 log "Untar du fichier des fichiers de config et maven"
 cd ~ 
