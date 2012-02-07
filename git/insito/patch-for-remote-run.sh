a7185c85519ee50a260cd083210df4e66ea17f46
Lescaroux Arnaud <alescaroux@financeactive.com>
2012-02-07 17:06:15 +0100

    Correction sudo avec commande multiples + replacement  de 'log' par 'echo'

--- git/insito/patch-for-remote-run.sh
+++ git/insito/patch-for-remote-run.sh
@@ -4,21 +4,19 @@
 # @author Cédric Pineau
 # Remote run
 
-SCRIPTS_DIRECTORY=~/.fa-toolkit
-. $SCRIPTS_DIRECTORY/git/safe-push/safe-commons
+echo "Installation de git"
+sudo apt-get update
+sudo apt-get install git
 
-
-log "Installation de git"
-sudo apt-get update && apt-get install git
-
-log "Installation de Protobuff"
+echo "Installation de Protobuff"
 
 if [ 0 -eq `grep http://rodolphe.quiedeville.org/debian /etc/apt/sources.list | wc -l `]; then
 	sudo sh -c 'echo "deb http://rodolphe.quiedeville.org/debian squeeze-backports main" >> /etc/apt/sources.list'
 fi
-sudo apt-get update && apt-get install libprotobuf-dev protobuf-compiler
+sudo apt-get update
+sudo apt-get install libprotobuf-dev protobuf-compiler
 
-log "Untar du fichier des fichiers de config et maven"
+echo "Untar du fichier des fichiers de config et maven"
 cd ~ 
 tar -xvf jboss.tar
 tar -xvf maven.tar
@@ -30,25 +28,25 @@ rm m2.tar
 mv bashrc .bashrc
 
 if [ ! -d ~/.ssh ]; then
-log "Création d'un répertoire ~/.ssh"
+echo "Création d'un répertoire ~/.ssh"
 mkdir ~/.ssh
 chmod 700 ~/.ssh
 fi
 
 if [ ! -d ~/remote-run ]; then
-	log "Préparation du repo git de remote run"
+	echo "Préparation du repo git de remote run"
 	mkdir ~/remote-run
 	cd ~/remote-run
 	git init .
 	git config receive.denyCurrentBranch ignore
 	git config receive.denyDeleteCurrent ignore
-	log "Repo créé sur ${HOME}/remote-run"
+	echo "Repo créé sur ${HOME}/remote-run"
 else
-	log "Vous avez déjà votre repo \"remote-run\""
+	echo "Vous avez déjà votre repo \"remote-run\""
 fi
 
 
-log "Préparation de la partie JBOSS"
+echo "Préparation de la partie JBOSS"
 cd ~/jboss-4.0.5.GA/server/insito/
 rm -rf ./insito-data
 ln -s ~/remote-run/data ./insito-data 
@@ -58,4 +56,4 @@ ln -s ~/remote-run/ear/ear/target/finactive-ear.ear ./deploy/insito.ear
 
 cd
 
-log "Installation OK"
+echo "Installation OK"
